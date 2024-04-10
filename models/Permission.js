71801957e23137f9af0eedd7311d6443d8acbd74
LenaVishnyakova <vishnyakova1213@gmail.com>
2024-04-10 22:40:22 +0900

    changed permission

--- models/Permission.js
+++ models/Permission.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 
 const PermissionSchema = new mongoose.Schema({
+    organization: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Organization'
+    },
     project: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Project'
+    },
+    stage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Stage'
     },
     role: {
         type: String,
